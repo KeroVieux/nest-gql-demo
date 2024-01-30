1cedf4439380f63c0ab40ff7c01dcb21002d63ba
小黄狗 <bigboss@hidoge.cn>
2024-01-31 01:15:52 +0800

    Update app.resolver.spec.ts

--- src/app.resolver.spec.ts
+++ src/app.resolver.spec.ts
@@ -2,11 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppResolver } from './app.resolver';
 import { Request } from 'express';
 import { AppService } from './app.service';
-
+// Here is a function to extend the Request interface.
+export interface RequestWithHeaders extends Request {
+  headers: {
+    'x-action': string;
+  };
+}
 describe('AppResolver', () => {
   let resolver: AppResolver;
   let appService: AppService;
-  const expectedUrl = 'http://localhost:3001';
+  const expectedUrl = 'http://localhost:3002';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,7 +20,12 @@ describe('AppResolver', () => {
         {
           provide: AppService,
           useValue: {
-            getBaseUrl: jest.fn().mockReturnValue(expectedUrl),
+            getBaseUrl: jest.fn((req: Request) => {
+              if (req.headers['x-action'] === 'payment') {
+                return 'http://localhost:3001';
+              }
+              return expectedUrl;
+            }),
           },
         },
       ],
@@ -26,10 +36,23 @@ describe('AppResolver', () => {
   });
 
   describe('getBaseUrl', () => {
-    it('should return the message from appService', () => {
-      const req: Request = {} as Request;
+    it('should return the message from appService without headers', () => {
+      const req: RequestWithHeaders = {
+        headers: { 'x-action': 'payment1' },
+      } as RequestWithHeaders;
       const context = { req };
-      jest.spyOn(appService, 'getBaseUrl').mockReturnValue(expectedUrl);
+      jest.spyOn(appService, 'getBaseUrl');
+      const result = resolver.getBaseUrl(context);
+
+      expect(result).toBe('http://localhost:3002');
+      expect(appService.getBaseUrl).toHaveBeenCalledWith(context.req);
+    });
+    it('should return the message from appService with headers', () => {
+      const req: RequestWithHeaders = {
+        headers: { 'x-action': 'payment' },
+      } as RequestWithHeaders;
+      const context = { req };
+      jest.spyOn(appService, 'getBaseUrl');
       const result = resolver.getBaseUrl(context);
 
       expect(result).toBe('http://localhost:3001');
